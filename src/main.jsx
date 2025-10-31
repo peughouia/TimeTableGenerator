30af2099d116e5c982eb9b6fa80ebaf53164f890
Peughouia <peughouia.44@gmail.com>
2025-10-31 22:31:18 +0100

    fix path v2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import TimeTableGenerator from './schedule.jsx'
+import { TimeTableGenerator } from './Schedules.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
